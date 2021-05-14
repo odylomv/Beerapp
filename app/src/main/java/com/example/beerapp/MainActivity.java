7771263904702c54b4d107ffbaafeb31225eca25
Chris <christpc@csd.auth.gr>
2021-05-14 16:20:24 +0300

    foundation (more soon)

---
+++ app/src/main/java/com/example/beerapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.beerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    /*public void openBeerCarousel (View view){
+        Intent intent = new Intent(this, BeerCarousel.class);
+        startActivity(intent);
+    }*/
+
+    public void openAbout(View view){
+        Intent intent = new Intent(this, About.class);
+        startActivity(intent);
+    }
+}
