006a8ead30d0c1c3b144d744ff85ac3d48313e31
alex <alexsofo@csd.auth.gr>
2021-05-28 17:17:25 +0300

    Favorite button heart works as a graphic

--- app/src/main/java/com/example/beerapp/BeerActivity.java
+++ app/src/main/java/com/example/beerapp/BeerActivity.java
@@ -1,13 +1,18 @@
 package com.example.beerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.bumptech.glide.Glide;
 
@@ -16,19 +21,25 @@ public class BeerActivity extends AppCompatActivity {
     private TextView aloneBeerName, aloneLongDesc;
     private ImageView aloneBeerPic;
     private Button addToFavBtn;
+    private ImageButton addToFvHeart;
+    private boolean isfavorite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beer);
+
         aloneBeerName = findViewById(R.id.aloneBeerName);
         aloneBeerPic = findViewById(R.id.aloneBeerpic);
         aloneLongDesc = findViewById(R.id.aloneLongDesc);
         addToFavBtn = findViewById(R.id.addtoFavBtn);
+        addToFvHeart = findViewById(R.id.addToFvHeart);
+
         //Beer beer = new Beer("Pipi", 0, "Short Pipi lolololo", "longPipi lololo", "https://www.csd.auth.gr/wp-content/uploads/2019/08/Tefas2020small-240x300.jpg");
+
         Beer beer = new Beer();
         Intent intent = getIntent();
-
+        //This is where we get each beer's content to display on Beer Activity using beerId and string arrays for content
         Resources res = getResources();
         String[] beerStyles = res.getStringArray(R.array.beerStyles);
         String [] beerImageLinks = res.getStringArray(R.array.beerImageLinks);
@@ -37,6 +48,24 @@ public class BeerActivity extends AppCompatActivity {
             int beerId = intent.getIntExtra(BEER_ID_KEY, -1); //TODO open specific beer for each tap create utilities class
             beer = new Beer(beerStyles[beerId],beerId,"This could be a short pipi", "This could be looong pipi",beerImageLinks[beerId]);
         }
+
+        addToFvHeart.setOnClickListener(new View.OnClickListener() {
+            /**
+             * This is only temporary till db is done
+             */
+            @Override
+            public void onClick(View view) {
+                if(!isfavorite){
+                    addToFvHeart.setBackgroundResource(R.drawable.ic_baseline_favorite_24);
+                    isfavorite = true;
+                }
+                else{
+                    addToFvHeart.setBackgroundResource(R.drawable.ic_baseline_favorite_border_24);
+                    isfavorite = false;
+                }
+            }
+        });
+
 
         setData(beer);
     }
