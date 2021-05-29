910b21b3a2e727978dd7fc1491d969c550615e4b
Chris <christpc@csd.auth.gr>
2021-05-29 15:42:34 +0300

    draft design

--- app/src/main/java/com/example/beerapp/MainActivity.java
+++ app/src/main/java/com/example/beerapp/MainActivity.java
@@ -1,25 +1,51 @@
 package com.example.beerapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.bumptech.glide.load.engine.Resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private Button exploreButton;
     private Button favButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        // Define ActionBar object
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
+
+        // Define ColorDrawable object and parse color
+        // using parseColor method
+        // with color hash code as its parameter
+        ColorDrawable colorDrawable
+                = new ColorDrawable(Color.parseColor("#e2d1c3"));
+
+        // Set BackgroundDrawable
+        actionBar.setBackgroundDrawable(colorDrawable);
+
+        Resources res = getResources();
+        String[] beerStyles = res.getStringArray(R.array.beerStyles);
+        String [] beerImageLinks = res.getStringArray(R.array.beerImageLinks);
+
 
         exploreButton = findViewById(R.id.buttonExplore);
         exploreButton.setOnClickListener(new View.OnClickListener() {
@@ -46,4 +72,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, About.class);
         startActivity(intent);
     }
+
 }
