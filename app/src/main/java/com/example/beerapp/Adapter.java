ee8455998c492e6fbb1b392355d8ce0d03f106a5
Chris <christpc@csd.auth.gr>
2021-05-30 12:39:41 +0300

    new icons - carousel fix

--- app/src/main/java/com/example/beerapp/Adapter.java
+++ app/src/main/java/com/example/beerapp/Adapter.java
@@ -10,7 +10,8 @@ import androidx.viewpager.widget.PagerAdapter;
 
 public class Adapter extends PagerAdapter {
     private final Context context;
-    private final int[] imageArray = new int[] { R.drawable.beer1, R.drawable.beer2, R.drawable.beer3 };
+    private final int[] imageArray = new int[] { R.drawable.beer1, R.drawable.beer2, R.drawable.beer3,R.drawable.beer4,R.drawable.beer5,R.drawable.beer6, //hard coded, probably best to initialize with a for loop
+            R.drawable.beer7,R.drawable.beer8,R.drawable.beer9,R.drawable.beer10,R.drawable.beer11,R.drawable.beer12,R.drawable.beer13,R.drawable.beer14};
 
     public Adapter(Context ctx) {
         context = ctx;
@@ -30,7 +31,7 @@ public class Adapter extends PagerAdapter {
     public Object instantiateItem(ViewGroup container, int position) {
         ImageView imageView = new ImageView(context);
         imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        imageView.setImageResource(imageArray[position % 3]);
+        imageView.setImageResource(imageArray[position % 14]);
         container.addView(imageView,0);
 
         return imageView;
@@ -38,6 +39,7 @@ public class Adapter extends PagerAdapter {
 
     @Override
     public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
-        container.removeView(container.getRootView());
+        //container.removeView(container.getRootView());
+        container.removeView((ImageView)object);
     }
 }
