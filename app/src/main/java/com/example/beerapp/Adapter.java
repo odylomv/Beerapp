e51ac147b6fe4c1b74cf935d4ea946947b91ffb9
odylomv <odylomv@gmail.com>
2021-05-30 13:14:42 +0300

    Carousel performance test

--- app/src/main/java/com/example/beerapp/Adapter.java
+++ app/src/main/java/com/example/beerapp/Adapter.java
@@ -10,8 +10,12 @@ import androidx.viewpager.widget.PagerAdapter;
 
 public class Adapter extends PagerAdapter {
     private final Context context;
-    private final int[] imageArray = new int[] { R.drawable.beer1, R.drawable.beer2, R.drawable.beer3,R.drawable.beer4,R.drawable.beer5,R.drawable.beer6, //hard coded, probably best to initialize with a for loop
-            R.drawable.beer7,R.drawable.beer8,R.drawable.beer9,R.drawable.beer10,R.drawable.beer11,R.drawable.beer12,R.drawable.beer13,R.drawable.beer14};
+    //hard coded, probably best to initialize with a for loop
+    private final int[] imageArray = { R.drawable.beer1, R.drawable.beer2, R.drawable.beer3, R.drawable.beer4,
+            R.drawable.beer5,  R.drawable.beer6, R.drawable.beer7, R.drawable.beer8, R.drawable.beer9,
+            R.drawable.beer10, R.drawable.beer11, R.drawable.beer12, R.drawable.beer13, R.drawable.beer14 };
+
+    private ImageView[] beerViews = new ImageView[imageArray.length];
 
     public Adapter(Context ctx) {
         context = ctx;
@@ -28,18 +32,22 @@ public class Adapter extends PagerAdapter {
     }
 
     @Override @NonNull
-    public Object instantiateItem(ViewGroup container, int position) {
-        ImageView imageView = new ImageView(context);
-        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        imageView.setImageResource(imageArray[position % 14]); //not future proof
-        container.addView(imageView,0);
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        int relativePos = position % imageArray.length;
 
-        return imageView;
+        if (beerViews[relativePos] == null) {
+            beerViews[relativePos] = new ImageView(context);
+            beerViews[relativePos].setScaleType(ImageView.ScaleType.CENTER_CROP);
+            beerViews[relativePos].setImageResource(imageArray[relativePos]);
+        }
+        container.addView(beerViews[relativePos],0);
+
+        return beerViews[relativePos];
     }
 
     @Override
     public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
         //container.removeView(container.getRootView());
-        container.removeView((ImageView)object);
+        container.removeView((ImageView) object);
     }
 }
